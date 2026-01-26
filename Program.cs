e15f3b1fb262ff35eb625cdb41e439cf7bed5cb2
Alessandro Taddei <alessandro.taddei@outlook.com>
2026-01-26 13:42:21 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj0000 {
+   internal class Program {
+      static void Main() {
+         Console.ForegroundColor = ConsoleColor.DarkCyan;
+         Console.WriteLine("**************************************************************************");
+         Console.WriteLine("           Calcular a área de círculos em metros quadrados (m²)           ");
+         Console.WriteLine("**************************************************************************\n");
+         Console.ResetColor();
+
+      start:
+         Console.Write("Digite o raio do círculo em metros ou \"sair\" para interromper: ");
+         string input = Console.ReadLine();
+         bool proceed = double.TryParse(input, out double radius);
+
+         if (input.ToLower() == "sair") {
+            Console.WriteLine();
+            return;
+         } else {
+            if (!proceed || radius < 0) {
+               IsInvalid();
+               goto start;
+            } else {
+               double area = Math.PI * Math.Pow(radius, 2);
+               Result(area, radius);
+               goto start;
+            }
+         }
+      }
+      static void IsInvalid() {
+         Console.ForegroundColor = ConsoleColor.DarkYellow;
+         Console.WriteLine("\nENTRADA INVÁLIDA! Tente novamente.");
+         Console.ResetColor();
+      }
+      static void Result(double a, double r) {
+         Console.Write($"\nA área do círculo de raio ");
+         Console.ForegroundColor = ConsoleColor.Red;
+         Console.Write($"{r}m");
+         Console.ResetColor();
+         Console.Write($" é igual a ");
+         Console.ForegroundColor = ConsoleColor.DarkGreen;
+         Console.Write($"{a}m²\n");
+         Console.ResetColor();
+      }
+
+   }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("prj0000")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("prj0000")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("84faec41-7ed7-44ce-8cb8-40227f4e940a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
